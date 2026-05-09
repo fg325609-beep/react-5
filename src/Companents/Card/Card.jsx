78912bfdd5124626996968bcbae76ea031267b60
Farhod Gofurov <fg325609@gmail.com>
2026-05-09 07:20:25 +0500

    lllll

--- src/Companents/Card/Card.jsx
+++ src/Companents/Card/Card.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div>
+        <form action="" method="post">
+            <label htmlFor="">ismingizni kirirting</label>
+            <input type="text" placeholder='ismingizni kirirting' />
+            <label htmlFor="">telefon raqamingizni kirirting</label>
+            <input type="number" placeholder='raqamingizni kirirting' />
+            <button type='submit'>jonatish</button>
+        </form>
+    </div>
   )
 }
 
